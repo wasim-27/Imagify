fed018cd22711a025ae0d22d4a1badb1f20ba98d
Mohammad Wasim Ansari <wasimansari11190348@gmail.com>
2026-05-29 19:22:57 +0530

    middleware added for credit balance

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -79,5 +79,26 @@ const loginUser=async (req,res)=>{
     }
 }
 
-export {registerUser,loginUser}
+const userCredits = async (req,res)=>{
+    try{
+        const {userId} = req.body
+
+        const user=await userModel.findById(userId)
+
+        res.json({
+            success:true,
+            credits:user.creditBalance,
+            user:{name:user.name}
+        })
+    }
+    catch(error){
+        console.log(error)
+        res.json({
+            success:false,
+            message:error.message
+        })
+    }
+}
+
+export {registerUser,loginUser,userCredits}
 
---
+++ server/middlewares/auth.js
@@ -0,0 +1,35 @@
+import jwt from 'jsonwebtoken'
+
+const userAuth =async(req,res,next) =>{
+    const {token} =req.headers;
+
+    if(!token){
+        return res.json({
+            success:false,
+            message:'Authentication failed..Login again'
+        });
+    }
+    try{
+        const tokenDecode=jwt.verify(token,process.env.JWT_SECRET);
+
+        if(tokenDecode.id){
+            req.body.userId=tokenDecode.id;
+        }
+        else{
+            return res.json({
+                success:false,
+                message:'Authentication failed..Login again'
+            });
+        }
+
+        next();
+    }
+    catch(error){
+        res.json({
+            success:false,
+            message: error.message
+        });
+    }
+}
+
+export default userAuth;
