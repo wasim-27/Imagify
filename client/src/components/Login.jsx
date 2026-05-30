e1a92d5022f2e1cbea8c1d931eac1e96c3e4ed84
Mohammad Wasim Ansari <wasimansari11190348@gmail.com>
2026-05-30 08:37:04 +0530

    Login page connect to backend, toastify used , axios used

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -2,11 +2,53 @@ import React, { useContext, useEffect, useState } from 'react'
 import { assets } from '../assets/assets'
 import { AppContext } from '../context/AppContext'
 import { motion } from "motion/react"
+import axios from 'axios'
+import { toast } from 'react-toastify'
 
 const Login = () => {
 
     const [state,setState]=useState('Login')
-    const {setShowLogin}=useContext(AppContext)
+    const {setShowLogin,backendUrl,setToken,setUser}=useContext(AppContext)
+
+    const [name, setName]=useState('')
+    const [email, setEmail]=useState('')
+    const [password, setPassword]=useState('')
+
+    const onSubmitHandler = async (e)=>{
+        e.preventDefault();
+        try{
+            if(state === 'Login'){
+                const {data} = await axios.post(backendUrl + '/api/user/login' , {email,password})
+
+                if(data.success){
+                    setToken(data.token)
+                    setUser(data.user)
+                    localStorage.setItem('token',data.token)
+                    setShowLogin(false)
+                }
+                else{
+                    toast.error(data.message)
+                }
+            }
+
+            else{
+                const {data} = await axios.post(backendUrl + '/api/user/register' , {name,email,password})
+
+                if(data.success){
+                    setToken(data.token)
+                    setUser(data.user)
+                    localStorage.setItem('token',data.token)
+                    setShowLogin(false)
+                }
+                else{
+                    toast.error(data.message)
+                }
+            }
+        }
+        catch(error){
+            toast.error(error.message)
+        }
+    }
 
     useEffect(()=>{
         document.body.style.overflow='hidden'; //disable the scrolling when login page is opened.
@@ -19,6 +61,7 @@ const Login = () => {
   return (
     <div className='fixed top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center'>
         <motion.form
+        onSubmit={onSubmitHandler}
         initial={{opacity:0.2,y:50}}
         transition={{duration:0.3}}
         whileInView={{opacity:1,y:0}}
@@ -27,21 +70,21 @@ const Login = () => {
             <h1 className='text-center text-2xl text-neutral-700 font-medium'>{state}</h1>
             <p className='text-sm'>Welcome back! Please sign in to continue</p>
 
-            {state!='Login' &&
+            {state !== 'Login' &&
             <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-5'>
                 <img src={assets.user_icon} alt="" />
-                <input className='outline-none text-sm' type="text" placeholder='Full Name' required />
+                <input onChange={e => setName(e.target.value)} value={name} className='outline-none text-sm' type="text" placeholder='Full Name' required />
             </div>
             }
 
             <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-4'>
                 <img src={assets.email_icon} alt="" />
-                <input className='outline-none text-sm' type="email" placeholder='Email id' required />
+                <input onChange={e => setEmail(e.target.value)} value={email} className='outline-none text-sm' type="email" placeholder='Email id' required />
             </div>
 
             <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-4'>
                 <img src={assets.lock_icon} alt="" />
-                <input className='outline-none text-sm' type="password" placeholder='Password' required />
+                <input onChange={e => setPassword(e.target.value)} value={password} className='outline-none text-sm' type="password" placeholder='Password' required />
             </div>
 
             <p className='text-sm text-blue-600 my-4 cursor-pointer'>Forgot Password?</p>
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -9,8 +9,14 @@ const AppContextProvider = (props)=>{
 
     const [showLogin,setShowLogin]=useState(false);
 
+    const [token,setToken]=useState(localStorage.getItem('token'))
+
+    const [credit,setCredit]=useState(false)
+
+    const backendUrl=import.meta.env.VITE_BACKEND_URL
+
     const value={
-        user,setUser,showLogin,setShowLogin
+        user,setUser,showLogin,setShowLogin,backendUrl,token,setToken,credit,setCredit
     }
     return (
         <AppContext.Provider value={value}>
